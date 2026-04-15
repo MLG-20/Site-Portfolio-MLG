94e7b3ba4990716739a990782e526ad90205f42b
MLG-20 <mlamine.gueye1@univ-thies.sn>
2026-04-15 18:41:10 +0000

    improve: enlarge admin button on mobile and add portfolio link in dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -8,12 +8,25 @@ use App\Filament\Widgets\RecentMessages;
 use App\Filament\Widgets\StatsOverview;
 use App\Filament\Widgets\TechByCategoryChart;
 use Filament\Pages\Dashboard as BaseDashboard;
+use Filament\Actions\Action;
 
 class Dashboard extends BaseDashboard
 {
     protected static ?string $navigationIcon = 'heroicon-o-home';
     protected static ?string $navigationLabel = 'Tableau de bord';
     protected static ?string $title = 'Tableau de bord';
+
+    public function getHeaderActions(): array
+    {
+        return [
+            Action::make('visit_portfolio')
+                ->label('📌 Voir le Portfolio')
+                ->url(url('/'))
+                ->openUrlInNewTab()
+                ->color('info')
+                ->icon('heroicon-o-arrow-top-right-on-square'),
+        ];
+    }
 
     public function getWidgets(): array
     {
